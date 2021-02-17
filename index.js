94ce93de2f6ccddb745b0aa56ae74f1c44093cee
Aric Lasry <lasry.aric@gmail.com>
2021-02-17 08:13:23 -0800

    Fix javascript exports

--- index.js
+++ index.js
@@ -3,4 +3,14 @@ import { NativeModules } from 'react-native';
 
 const { RNReactNativeOgury } = NativeModules;
 
-export default RNReactNativeOgury;
+const RNOguryBanner320x50 = requireNativeComponent('RNOguryBanner320x50');
+const RNOguryBannerMPU = requireNativeComponent('RNOguryBannerMPU');
+
+
+module.exports = {
+  RNReactNativeOgury: RNReactNativeOgury,
+  RNOguryBanner320x50: RNOguryBanner320x50,
+  RNOguryBannerMPU: RNOguryBannerMPU
+  
+  
+}
