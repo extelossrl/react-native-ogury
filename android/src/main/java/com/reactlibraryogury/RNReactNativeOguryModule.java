26b78343e0d660258a9c3b36b63b386c737c8467
Tanguy de La Villegeorges <tvillegeorges@gmail.com>
2021-02-18 17:35:00 +0100

    choice manager set up

--- android/src/main/java/com/reactlibraryogury/RNReactNativeOguryModule.java
+++ android/src/main/java/com/reactlibraryogury/RNReactNativeOguryModule.java
@@ -1,4 +1,3 @@
-
 package com.reactlibraryogury;
 
 import android.content.Context;
@@ -13,6 +12,9 @@ import com.ogury.ed.OguryBannerAdSize;
 import com.ogury.ed.OguryBannerAdView;
 import com.ogury.sdk.OguryConfiguration;
 import com.ogury.sdk.Ogury;
+import com.ogury.cm.OguryChoiceManagerExternal;
+import com.ogury.cm.OguryChoiceManager;
+import com.ogury.cm.OguryConsentListener;
 
 public class RNReactNativeOguryModule extends ReactContextBaseJavaModule {
 
@@ -30,9 +32,30 @@ public class RNReactNativeOguryModule extends ReactContextBaseJavaModule {
     OguryConfiguration.Builder oguryConfigurationBuilder;
     oguryConfigurationBuilder = new OguryConfiguration.Builder(this.reactContext, appKey);
     Ogury.start(oguryConfigurationBuilder.build());
-
-
+    // Without choice manager
+    //Integer[] nonIabVendorsAccepted = new Integer[0];
+    //OguryChoiceManagerExternal.TcfV2.setConsent(this.reactContext, appKey, iabString, nonIabVendorsAccepted);
+    // With choice manager
+    OguryChoiceManager.ask(getCurrentActivity(), oguryConsentListener);
+    //OguryChoiceManager.edit(getCurrentActivity(), oguryConsentListener);
   }
+
+  private final OguryConsentListener oguryConsentListener = new OguryConsentListener() {
+    @Override public void onComplete(OguryChoiceManager.Answer answer) {
+      // transmit consent to other vendors' SDKs
+      Log.e("Ogury", "oguryConsentListener");
+      String consentAnswer = String.valueOf(answer);
+      Log.e("Ogury consent answer", consentAnswer);
+      String iabString = OguryChoiceManager.TcfV2.getIabString();
+      Log.e("Ogury IAB", iabString);
+      boolean gdprAccepted = OguryChoiceManager.gdprApplies();
+      Log.e("Ogury gdprApplies", String.valueOf(gdprAccepted));
+    }
+    @Override public void onError(OguryError error) {
+      // handle error
+      Log.e("Ogury", "transmit consent error");
+    }
+  };
   @Override
   public String getName() {
     return "RNReactNativeOgury";
